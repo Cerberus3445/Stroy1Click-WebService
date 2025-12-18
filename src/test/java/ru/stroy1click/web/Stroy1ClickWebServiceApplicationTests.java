d11024e6c145b42526a86b90d19681179b0fb7af
Egor <egormalov.2007@gmail.com>
2025-12-18 21:01:19 +0300

    Initial commit

---
+++ src/test/java/ru/stroy1click/web/Stroy1ClickWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stroy1click.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stroy1ClickWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
